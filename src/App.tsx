8cdeebebc284d9cc03f32bcfc987331e7b0ac13d
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-28 11:01:52 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,9 @@ import { MovieProvider } from 'movies/components/MovieProvider';
 
 function App() {
   return (
-    <div className="App">
-      <Header title='React Movie Management Application'/>
-      <div className="mt-5">
-        <MovieProvider>
-          <div className="container-fluid">
-            <div className="d-flex flex-row">
-              <div className="col-sm-12">
-                <MovieList/>
-              </div>
-            </div>
-          </div>
-        </MovieProvider>
-      </div>
-    </div>
+    <MovieProvider>
+        <MovieList/>
+    </MovieProvider>
   );
 }
 
