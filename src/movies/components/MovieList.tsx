7b4c31c450462d1e5e2c430e12a6beebdf0d1e14
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-28 11:04:53 +0200

    Update MovieList.tsx

--- src/movies/components/MovieList.tsx
+++ src/movies/components/MovieList.tsx
@@ -12,25 +12,24 @@ type NewMovieMode = "BUTTON" | "FORM"
 export const MovieList = () => {
   const { movies, moviesDispatch } = useMovies();
   const [displayOptionType, setDisplayOptionType] = useState<NewMovieMode>('BUTTON');
-
-  // TODO: Display list of movies 
+  
   return (
-    <div className="card-deck">
+    <>
       {movies.map(movie => (
         <Card key={movie.id}>
           <MovieCard key={movie.id} movie={movie} />
         </Card>      
       ))}
-      <Card>
-        {/* TODO: Implement displaying appropriate card for add movie - button or form */}
-        {/* TODO: use AddMovieButton and AddMovieForm */}
+      
+      <Card>  
         {displayOptionType === "BUTTON" ? <AddMovieButton onClick={() => setDisplayOptionType("FORM")}/> : null}
+
         {displayOptionType === "FORM" ? <AddMovieForm onSubmit={({imageUrl, title, description, subtitle}) => {
           const movie = {title: title, subtitle: subtitle, description: description, imageUrl: imageUrl, ratings: [] };
           moviesDispatch({type: "add", payload: { movie: movie }})
           setDisplayOptionType("BUTTON");
         }} onCancel={() => setDisplayOptionType("BUTTON")} /> : null}
       </Card>
-    </div>
+    </>
   );
 }
