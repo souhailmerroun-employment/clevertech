58a98ac40912efe2fb7d7297ca5c4aff90ded767
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-28 11:02:50 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import './App.css';
-
-import { Header } from './shared/components';
 import { MovieList } from './movies/components';
 import { MovieProvider } from 'movies/components/MovieProvider';
 
 function App() {
   return (
     <MovieProvider>
-        <MovieList/>
+      <MovieList/>
     </MovieProvider>
   );
 }
